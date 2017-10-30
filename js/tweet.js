333b8654e6d96c220a3cc1efac653f4876dbe23c
Ben Fedidat <fedidat@gmail.com>
2017-10-30 17:16:09 +0200

    improved function on ofekquery

--- js/tweet.js
+++ js/tweet.js
@@ -1,39 +1,34 @@
-(function(){
-    
-    var tweets = [
-         {username: 'Bobo', text: 'hello followers!'},
-         {username: 'Elvis', text: 'this exercise is really easy!'},
-         {username: 'Mimi', text: 'hello!'}
-    ]; 
+var tweets = [
+     {username: 'Bobo', text: 'hello followers!'},
+     {username: 'Elvis', text: 'this exercise is really easy!'},
+     {username: 'Mimi', text: 'hello!'}
+]; 
 
-    function loadTweets() {
-        tweetsContainer = document.getElementById("tweets");
-        tweets.forEach(tweet => { 
-            appendTweet(getUserImage(tweet.username), tweet.username, tweet.text);
-        });
-    }
+function loadTweets() {
+    tweets.forEach(tweet => { 
+        appendTweet(getUserImage(tweet.username), tweet.username, tweet.text);
+    });
+}
 
-    function submitTweet() {
-        var messageTextArea = document.getElementById("tweetBody");
-        var message = messageTextArea.value.replace(/\r?\n/g, '<br />');
-        tweets.push({username: placeholderCurrentUsername, text: message});
-        appendTweet(getUserImage(placeholderCurrentUsername), placeholderCurrentUsername, message);
-        messageTextArea.value = "";
-    }
+function submitTweet() {
+    var message = $("#tweetBody").get(0).value.replace(/\r?\n/g, '<br />');
+    tweets.push({username: placeholderCurrentUsername, text: message});
+    appendTweet(getUserImage(placeholderCurrentUsername), placeholderCurrentUsername, message);
+    $("#tweetBody").get(0).value = "";
+}
 
-    function appendTweet(imagePath, username, message) {
-        var tweetDiv = document.createElement("div");
-        tweetDiv.innerHTML = `
-            <li class="media my-3">
-                <img src="${imagePath}" class="img-thumbnail mr-3">
-                <div class="media-body">
-                    <strong>${username}</strong>
-                    <p>${message}</p>
-                </div>
-            </li>
-            `;
-        document.getElementById("tweets").appendChild(tweetDiv);
-    }
+function appendTweet(imagePath, username, message) {
+    var tweetDiv = document.createElement("div");
+    tweetDiv.innerHTML = `
+        <li class="media my-3">
+            <img src="${imagePath}" class="img-thumbnail mr-3">
+            <div class="media-body">
+                <strong>${username}</strong>
+                <p>${message}</p>
+            </div>
+        </li>
+        `;
+    $("#tweets").appendChild(tweetDiv);
+}
 
-    loadTweets();
-})()
+loadTweets();
